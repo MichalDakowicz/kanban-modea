6f16904708b3fede9bd511fa671c1e4f302a4da6
Michu <michal.dakowicz@infotech.edu.pl>
2026-04-21 20:47:40 +0200

    Add initial configuration and setup for Kanban application
    - Created config.example.js with Firebase configuration and team members data.
    - Added database.rules.json for Firebase database rules.
    - Introduced firebase.json for Firebase hosting configuration.
    - Implemented index.html for the Kanban login interface with user selection and password validation.

---
+++ config.example.js
@@ -0,0 +1,54 @@
+const FIREBASE_CONFIG = {
+    apiKey: "<FIREBASE_API_KEY>",
+    authDomain: "<FIREBASE_AUTH_DOMAIN>",
+    projectId: "<FIREBASE_PROJECT_ID>",
+    storageBucket: "<FIREBASE_STORAGE_BUCKET>",
+    messagingSenderId: "<FIREBASE_MESSAGING_SENDER_ID>",
+    appId: "<FIREBASE_APP_ID>",
+    measurementId: "<FIREBASE_MEASUREMENT_ID>",
+    databaseURL: "<FIREBASE_DATABASE_URL>",
+};
+
+const APP_PASSWORD = "<APP_PASSWORD>";
+
+const TEAM_MEMBERS = [
+    {
+        id: "michal.dakowicz",
+        name: "Michał Dakowicz",
+        initials: "MD",
+        color: "#185FA5",
+    },
+    {
+        id: "borys.borejszo",
+        name: "Borys Borejszo",
+        initials: "BB",
+        color: "#362300",
+    },
+    {
+        id: "jakub.zawadzki",
+        name: "Jakub Zawadzki",
+        initials: "JZ",
+        color: "#0F6E56",
+    },
+    {
+        id: "krzysztof.rusel",
+        name: "Krzysztof Rusel",
+        initials: "KR",
+        color: "#993C1D",
+    },
+    {
+        id: "maciej.mocarski",
+        name: "Maciej Mocarski",
+        initials: "MM",
+        color: "#ab06b1",
+    },
+    {
+        id: "oskar.topolewicz",
+        name: "Oskar Topolewicz",
+        initials: "OT",
+        color: "#6e0f2f",
+    },
+];
+
+const SECTIONS = ["Frontend", "Backend", "Mobile", "Docs"];
+const COLUMNS = ["Backlog", "Do zrobienia", "W trakcie", "Review", "Zrobione"];
